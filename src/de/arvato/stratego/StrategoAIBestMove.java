b809759c1848f085d3390c5f8e0198f607f3c398
gome016 <ivan.gomez1@arvato.com>
2020-04-25 19:06:33 +0200

    Clean-UP

--- src/de/arvato/stratego/StrategoAIBestMove.java
+++
@@ -1,7 +0,0 @@
-package de.arvato.stratego;
-
-public class StrategoAIBestMove {
-
-	public int [] bestMove;
-	public int bestScore;
-}
--- src/de/arvato/stratego/StrategoAIListener.java
+++
@@ -1,9 +0,0 @@
-package de.arvato.stratego;
-
-import javafx.util.Pair;
-
-public interface StrategoAIListener {
-
-	public void moveAI (int from, int to);
-	
-}
